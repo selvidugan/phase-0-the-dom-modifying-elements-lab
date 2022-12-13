33d78357788c41d92dc05b1105f6a9e50a86a487
selvidugan <selvidugan@gmail.com>
2022-12-13 18:48:17 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+
+const main = document.getElementById('main');
+main.remove();
+const newHeader = document.createElement('h1');
+newHeader.setAttribute('id','victory');
+newHeader.innerHTML = "SELVİ is the champion";
+document.head.appendChild(element);
